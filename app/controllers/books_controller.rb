e1dc9a4f7a00c343aaf03b5d371c61bf259c1cc7
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-06-11 09:38:30 +0000

    [Add] Bookers-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,24 +4,38 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(list_params)
+    book = Book.new(book_params)
     book.save
-    redirect_to '/books/:id'
+    redirect_to book_path(book.id)
   end
 
   def index
     @books = Book.all
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book.id)
+  end
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
 
-  def list_params
+  def book_params
     params.require(:book).permit(:title,:body)
   end
 end
